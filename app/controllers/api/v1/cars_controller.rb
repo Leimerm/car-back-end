96494c6ad4f9705a3172d62bcd647344454bc5f9
Leimerm <matthewleimer@gmail.com>
2020-10-10 19:37:02 -0500

    Fix

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::CarsController < ApplicationController
     #GET /cars/1
     def show 
         @cars = Car.where(car_id: params[:id])
-        render json: {car :@car}
+        render json {car :@car}
     end
 
     #POST /cars
@@ -19,7 +19,7 @@ class Api::V1::CarsController < ApplicationController
         if @car.save
             render json: @car
         else 
-            render json: @car.errors, status: :unprocessable_entity
+            render json @car.errors, status: :unprocessable_entity
         end
 
     end
